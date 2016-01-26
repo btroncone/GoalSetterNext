090e40e076ded9be2ccdaff4333556fd2297b91a
Brian Troncone <btroncone@gmail.com>
2016-01-25 22:40:37 -0500

    refactoring project for ngrx

--- app/services/goal-service.ts
+++ app/services/goal-service.ts
@@ -1,63 +1,26 @@
-import {Storage, SqlStorage } from "ionic-framework/ionic";
-import {toArray} from "lodash";
-/* It doesn't appear momentjs is set up to import as ES6 module so we have to use 'require'*/
+import { Storage, LocalStorage } from "ionic-framework/ionic";
 const moment = require("moment");
 
-interface SQLLiteResponse{
-    res: {
-        rows: Array<Object>
-    },
-    txt: any
+export interface Goal {
+    description: string,
+    status: boolean,
+    date: string
 }
 
 export class GoalService{
-    private _goalDb : SqlStorage;
+    private _goalDb : LocalStorage;
     
     constructor(){
-        this._goalDb = new Storage(SqlStorage);
-        this._goalDb.query('CREATE TABLE IF NOT EXISTS Goals (id INTEGER PRIMARY KEY AUTOINCREMENT, goal TEXT, complete INTEGER, date TEXT)').
-            then(data => {
-                console.log("TABLE CREATED!");
-            }, error => {
-                console.log("ERROR CREATING TABLE!");
-            });      
+        this._goalDb = new Storage(LocalStorage);
     }
     
-    insertGoal({goal, complete, date}){
-        return this._goalDb.query("INSERT INTO Goals (goal, complete, date) VALUES (?, ?, ?)", 
-            [goal, complete, moment(date).format('L')]).
-                then((data : SQLLiteResponse) => {
-                    return toArray(data.res.rows);
-                }, (error) => {
-                    console.log(error);
-                });
-    }
-    
-    updateGoalStatus({id, status}){
-        return this._goalDb.query("UPDATE Goals SET complete = ? WHERE id = ?", 
-            [id, status]).
-                then((data : SQLLiteResponse) => {
-                    return toArray(data.res.rows);
-                }, (error) => {
-                    console.log(error);
-                });
-    }
-    
-    deleteGoal(id: number){
-        return this._goalDb.query(`DELETE From Goals WHERE id = ?`, [id]).
-            then((data : SQLLiteResponse) => {
-                return toArray(data.res.rows);
-            }, (error) => {
-                //display error
-            });
+    setGoals(date : string, goals : Goal[]){
+        return this._goalDb.set(date, JSON.stringify(goals));
     }
     
     retrieveGoalsByDate(date: string){
-        return this._goalDb.query(`SELECT * FROM Goals WHERE date = ?`, [date]).
-            then((data : SQLLiteResponse) => {
-                return toArray(data.res.rows);
-            }, (error) => {
-                //display error
-            });
+        return this._goalDb.get(date).then(goals => {
+          return goals ? JSON.parse(goals) : [];  
+        });       
     }
 }
